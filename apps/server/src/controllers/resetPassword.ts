8e2466fc651234c73c45674475ff7494083d1f22
Priyesh2025 <tandelp000@gmail.com>
2023-09-29 12:32:05 +0530

    Structured controller/resetPassword.ts

--- apps/server/src/controllers/resetPassword.ts
+++ apps/server/src/controllers/resetPassword.ts
@@ -3,6 +3,8 @@ import { Request, Response } from "express";
 import { db } from "../db";
 import { users } from "../db/schema";
 import { eq } from "drizzle-orm";
+import { createTokenUserJWT } from '../utils/createTokenUser'
+import { attachCookiesToResponse } from '../utils/attachCookiesToResponse'
 import { randomBytes } from "crypto";
 import { sendVerificationEmail } from "../utils/sendVerificationEmail";
 import bcrypt from "bcrypt";
@@ -17,18 +19,36 @@ export const resetPasswordGet = async (req: Request, res: Response) => {
 };
 
 
-export const resetPasswordPost = async (req: Request, res: Response) => {
-  var { email} = req.body;
+export const resetPassword = async (req : Request, res : Response) => {
+  const { token, email, password } = req.body;
+  if (!token || !email || !password) {
+    return res.status(400).send({ error: "Invalid/insufficient email or Password or token" });
+  }
+  try {
 
-  if (!email) {
-    return res.status(400).send({ error: "emailId required" });
+    const User = await db
+      .select()
+      .from(users)
+      .where(eq(users.emailId, email))
+      .limit(1);
+
+    if (User.length<1) {
+      return res.status(400).send({ error: "Invalid Credentials" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, User[0].password);
+    if (!isPasswordCorrect) {
+      return res.status(400).send({ error: "Invalid Credentials" });
+    }
+
+    const userJwtToken = createTokenUserJWT(User[0].id);
+    
+    attachCookiesToResponse(res, userJwtToken );
+
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ message: "Internal server error" });
   }
 
-
-  
-
-  res.send({ message: "Password reset Successful." });
+res.send('reset password');
 };
-
-
-
