236db7cf0b0a3aa742527c38ad43bef321581671
Viraj <virajrathva1310@gmail.com>
2023-10-31 18:59:54 +0530

    Email_invitation done

---
+++ apps/server/src/view/Email_Invitation.ts
@@ -0,0 +1,137 @@
+export const emailBody =({
+
+}:{
+
+}): string => {
+
+  return `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <meta name="x-apple-disable-message-reformatting" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
+        rel="stylesheet"
+      />
+      <title></title>
+      <!--[if mso]>
+        <noscript>
+          <xml>
+            <o:OfficeDocumentSettings>
+              <o:PixelsPerInch>96</o:PixelsPerInch>
+            </o:OfficeDocumentSettings>
+          </xml>
+        </noscript>
+      <![endif]-->
+      <style>
+        table,
+        td,
+        div,
+        h1,
+        p {
+          font-family: "Montserrat", sans-serif;
+        }
+      </style>
+    </head>
+    <body
+      style="
+        overflow: hidden;
+        height: 100vh;
+        margin-left:0;
+        margin-right:0;
+        margin-top:0;  
+      "
+    >
+
+    <table style="background:#fda08b;
+    width: 100%;
+    height: 100vh;
+    justify-items: center;
+    background-size: contain;
+    border-spacing: 0;
+    border-collapse: collapse;
+    border: 0;
+    text-align:center;
+    
+    
+    ">
+
+   
+    <tr style="height: 100px;">
+      <td  style="  
+      display: flex;
+      margin: auto;
+      padding-left:10px;
+     ">
+
+    <img  src="https://imageupload.io/ib/ishrKlw4Gn4wAwi_1698256966.png"    width="100px" height="50px" >
+        
+    </td>
+    </tr>
+    
+    <tr>
+    <td style="height: 50px;">
+      <h1 style="font-weight: bold;margin-top: 10px;margin-bottom: 0px;padding-left:10px; color:black">Welcome to TEEM! </h1>
+    </td>
+    </tr>
+
+    <tr>
+      <td style="padding: 0px; height: 80px;">
+        <p style="margin-top: 10px;margin-bottom: 0px; padding-left:10px; color:black">@user has invited you to join them in the TEEM-app</p>
+      </td>
+    </tr>
+
+    <tr>
+      <td style="height: 30px;">
+        <p style="font-size: medium;margin-top: 0px;margin-bottom: 25px; padding-left:10px; color:black">Join them on TEEM to collaborate, manage <br>
+            projects and view what your team is doing.
+        </p>
+        
+      </td>
+    </tr>
+
+    <tr>
+      <td  style=" padding-left: 10px;
+      cursor: pointer;
+      border: 0;
+      height:60px;
+      ">
+       <button style="background-color: #000000;  width: 180px;height: 40px; border-radius:10px; cursor: pointer; color:white; border: 0px"> Accept Invite </button>
+      </td>
+    </tr>
+
+    <tr>
+        <td style="height: 60px;">
+            <p style="font-weight: 200;margin-top: 0px;margin-bottom: 0px;  padding-left:10px; color:black"> <b>What is TEEM App?</b>  Project tracking <a style="font-weight: 100;" href="text" >Learn more</a></p>
+        </td>
+    </tr>
+
+
+    <tr>
+        <td style=" display: flex;
+        justify-content: center;
+        font-style: normal;
+        font-weight: 500;
+        font-size: medium;
+        color: black; 
+        ">
+            <p>This message was sent to you by TEEM</p>
+        </td>
+    </tr>
+
+    <tr>
+      <td style="vertical-align:bottom;position: absolute; right: 0; bottom: 0;  ">
+        <img src="https://imageupload.io/ib/OueRP7UrcSrON8O_1698412000.png" alt="doodle.png" width="250px" height="250px" >
+      </td>
+    </tr>
+   </table> 
+
+  
+</body>
+</html>`;
+
+};
+
