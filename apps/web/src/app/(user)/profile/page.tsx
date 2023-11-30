ae33d94c756a070880234709cab093331c9608de
DhyeyV <dhyeyvachhani569@gmail.com>
2023-11-30 21:27:25 +0530

    Navbar added

--- apps/web/src/app/(user)/profile/page.tsx
+++ apps/web/src/app/(user)/profile/page.tsx
@@ -1,13 +1,14 @@
+"use client";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 import NavComponent from "@/components/Navbar";
 import UserProfile from "../components/user-profile";
-
+import NavBar from "@/components/newNavbar";
 export default function Profilepage() {
   return (
     <div className="h-screen w-screen overflow-hidden bg-[#E5F2FF] flex flex-col relative">
-      <NavComponent />
+      <NavBar />
       <div className="flex-grow flex items-center justify-center border-2">
         <div className="h-4/5 my-auto md:w-4/5 xl:w-1/3 mx-auto bg-white rounded-2xl shadow-xl flex flex-col items-center justify-evenly pb-5">
           <div className="h-24 sm:w-15 md:w-20 lg:w-24 flex items-center justify-center rounded-full bg-[#2222223b]">
--- apps/web/src/components/ui/task-dialog.tsx
+++ apps/web/src/components/ui/task-dialog.tsx
@@ -1,16 +1,24 @@
 import Task from "@/components/CreateTask";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import Image from "next/image";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export function TaskDialog() {
   return (
     <Dialog>
       <DialogTrigger asChild className="cursor-pointer">
-        <Image
-          src={"/img/add_icon.svg"}
+        {/* <Image
+          src={"/img/add_icon_white.png"}
           alt="Image Not found"
           width={28}
           height={28}
+        /> */}
+
+        <FontAwesomeIcon
+          className="hover:scale-110 md:hover:text-blue-300"
+          icon={faPlus}
+          height={30}
         />
       </DialogTrigger>
       <DialogContent className="max-w-[1000px] h-4/5 bg-[#E5F2FF] ">
